2d240ad2e7e4c70dcf56ba406313a995b574a6f7
Fred <thecanuckatheist@gmail.com>
2019-10-17 14:11:03 +0000

    added forEach

--- findWaldo.js
+++ findWaldo.js
@@ -1,16 +1,25 @@
 // The second argument/parameter is expected to be a (callback) function
-const findWaldo = function(names, found) {
-  for (let i = 0; i < names.length; i++) {
-    let name = names[i];
-    if (name === "Waldo") {
-      let index = i;
-      found(index);   // execute callback
-    }
-  }
-}
+// const findWaldo = function(names, found) {
+//   for (let i = 0; i < names.length; i++) {
+//     let name = names[i];
+//     if (name === "Waldo") {
+//       let index = i;
+//       found(index);   // execute callback
+//     }
+//   }
+// }
 
 const actionWhenFound = function(index) {
   console.log("Found Waldo at index " + index + "!");
 }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+let findWaldo = ["Alice", "Bob", "Waldo", "Winston"];
+let index = 0;
+
+findWaldo.forEach(function(element){
+  let name = element;
+  if (name === "Waldo") {
+    actionWhenFound(index);
+  }
+  index++;
+}, actionWhenFound);
